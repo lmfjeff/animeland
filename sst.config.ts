026416133a3fe0cd7689fbb61b6b31ada96bc890
lmfjeff <lmfjeff@gmail.com>
2023-12-06 00:44:30 +0800

    format once & add auto format for staged file

--- sst.config.ts
+++ sst.config.ts
@@ -1,20 +1,20 @@
-import { SSTConfig } from "sst";
-import { NextjsSite } from "sst/constructs";
+import { SSTConfig } from "sst"
+import { NextjsSite } from "sst/constructs"
 
 export default {
   config(_input) {
     return {
       name: "animeland",
       region: "ap-northeast-1",
-    };
+    }
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+      const site = new NextjsSite(stack, "site")
 
       stack.addOutputs({
         SiteUrl: site.url,
-      });
-    });
+      })
+    })
   },
-} satisfies SSTConfig;
+} satisfies SSTConfig
