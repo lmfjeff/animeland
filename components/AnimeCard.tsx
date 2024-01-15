fe068b6b3da9975e71826b6678eed3f09e03ba72
lmfjeff <lmfjeff@gmail.com>
2024-01-15 15:46:54 +0800

    wip

--- components/AnimeCard.tsx
+++ components/AnimeCard.tsx
@@ -6,16 +6,16 @@ const NOT_SHOW_IMG = 1
 
 export default function AnimeCard({ anime, sort }) {
   return (
-    <div className="border border-gray-300 flex flex-col">
-      <div className="border-b relative">
+    <div className="flex flex-col">
+      <div className="relative">
         <Link href={`/anime/${anime.id}`}>
-          <img src={NOT_SHOW_IMG ? "" : anime?.images?.[0]?.lg} className="aspect-square object-cover" />
+          <img src={NOT_SHOW_IMG ? "" : anime?.images?.[0]?.lg} className="w-full aspect-square object-cover" />
         </Link>
-        <div className="absolute top-0 left-0 bottom-0 right-0 flex flex-col">
+        {/* <div className="absolute top-0 left-0 bottom-0 right-0 flex flex-col">
           <Button>follow</Button>
           <Button>rate</Button>
           <Button>status</Button>
-        </div>
+        </div> */}
       </div>
 
       <div className="bg-blue-100">
