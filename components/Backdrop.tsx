61c3bc270baad86ef27f23cb98efc76086e31715
lmfjeff <lmfjeff@gmail.com>
2024-01-22 11:13:45 +0800

    lock scroll but show scrollbar to prevent layout shift

--- components/Backdrop.tsx
+++ components/Backdrop.tsx
@@ -10,8 +10,20 @@ export default function Backdrop() {
   function onClose() {
     router.back()
   }
-  // todo lock scroll but show scrollbar to prevent layout shift
-  useLockBodyScroll()
+  useEffect(() => {
+    if (window.innerHeight < document.body.scrollHeight) {
+      const top = window.scrollY
+      document.body.style.position = "fixed"
+      document.body.style.overflowY = "scroll"
+      document.body.style.width = "100%"
+      document.body.style.top = `-${top}px`
+    }
+    return () => {
+      document.body.style.position = "static"
+      document.body.style.overflowY = "auto"
+      document.body.style.top = "0px"
+    }
+  }, [])
 
   useHotkeys("esc", onClose)
   return <div className="z-backdrop absolute w-full h-full bg-[rgba(0,0,0,0.5)]" onClick={onClose}></div>
